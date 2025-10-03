2a0fc00fb0470e116eb88ee5bf53a05dc529763b
Danny Ospino <dannii.ospino@gmail.com>
2025-10-02 21:59:27 -0500

    wedding ready

--- src/app/pages/wedding-invitation-card/wedding-invitation-card.component.ts
+++ src/app/pages/wedding-invitation-card/wedding-invitation-card.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { LoadingComponent } from '../../shared/components/loading/loading.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-wedding-invitation-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoadingComponent, RouterLink],
   templateUrl: './wedding-invitation-card.component.html',
   styleUrl: './wedding-invitation-card.component.scss'
 })
