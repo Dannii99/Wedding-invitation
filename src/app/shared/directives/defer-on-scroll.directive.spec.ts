284d6b0847bd7cdc6bff07e354ef74c93463d181
Danny Ospino <dannii.ospino@gmail.com>
2026-01-03 19:22:45 -0500

    lazy loading animate

---
+++ src/app/shared/directives/defer-on-scroll.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DeferOnScrollDirective } from './defer-on-scroll.directive';
+
+describe('DeferOnScrollDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DeferOnScrollDirective();
+    expect(directive).toBeTruthy();
+  });
+});
